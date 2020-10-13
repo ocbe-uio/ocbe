4d60cf15a50effb1a40def4e78822ecf4a34781d
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-10-13 12:15:06 +0200

    Added package help file

--- R/ocbe.R
+++ R/ocbe.R
@@ -1,7 +1,7 @@
 #' @title Misc functions from OCBE/UiO and OCBE/OUS
-#' @description This package contains miscellaneous R functions created by 
+#' @description This package contains miscellaneous R functions created by
 #'  staff members of the Oslo Centre for Biostatistics and Epidemiology (OCBE).
-#'  OCBE is a joint center between the University of Oslo and the Oslo 
+#'  OCBE is a joint center between the University of Oslo and the Oslo
 #'  University Hospital.
 #' @docType package
 #' @name ocbe
